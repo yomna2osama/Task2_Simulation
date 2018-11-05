84785836bc33c81ac615c5ad6f6c8662e9a0d833
maioy97 <mai.algendyfcis@gmail.com>
2018-11-05 15:54:09 +0200

    simulation case done

--- NewspaperSellerModels/SimulationCase.cs
+++ NewspaperSellerModels/SimulationCase.cs
@@ -18,5 +18,47 @@ namespace NewspaperSellerModels
         public decimal LostProfit { get; set; }
         public decimal ScrapProfit { get; set; }
         public decimal DailyNetProfit { get; set; }
+
+        public void set_dem_type(int dayno, ref DemandDistribution[] dem_distable, ref DayTypeDistribution[] day_distable)
+        {
+            this.DayNo = dayno;
+            Random rand = new Random();
+            this.RandomNewsDayType = rand.Next(1, 101);
+            this.RandomDemand = rand.Next(1, 101);
+            for (int i = 0; i < day_distable.Length; i++)
+            {
+                if (this.RandomNewsDayType >= day_distable[i].MinRange && this.RandomNewsDayType < day_distable[i].MaxRange)
+                {
+                    this.NewsDayType = day_distable[i].DayType;
+                    break;
+                }
+            }
+            for (int i = 0; i < dem_distable.Length; i++)
+            {
+                    if (this.RandomDemand >= dem_distable[i].DayTypeDistributions[(int)this.NewsDayType].MinRange && this.RandomDemand < dem_distable[i].DayTypeDistributions[(int)this.NewsDayType].MaxRange)
+                    {
+                        this.Demand = dem_distable[i].Demand;
+                        break;
+                    }
+            }
+        }
+        public void calculate_costs(int Supply, int NumOfRecords, decimal PurchaseP, decimal ScrapP, decimal SellingP)
+        {
+            this.DailyCost = Supply * PurchaseP;
+            if (Demand >= Supply)
+            {
+                this.SalesProfit = Supply * SellingP;
+                this.LostProfit = (Demand - Supply) * (SellingP - PurchaseP);
+                this.ScrapProfit = 0;
+            }
+            if (Demand < Supply)
+            {
+                this.SalesProfit = Supply * SellingP;
+                this.LostProfit = 0;
+                this.ScrapProfit = (Supply - Demand) * ScrapP;
+            }
+            this.DailyNetProfit = this.SalesProfit - this.DailyCost - this.LostProfit + this.ScrapProfit;
+        }
+        
     }
 }
